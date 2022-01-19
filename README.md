b1c5252002d0260ad496d3ee565bab0ec95bc3ef
Glitch (glitch-hello-node) <none>
2022-01-19 03:42:29 +0000

    ☃️🌇 Checkpoint ./README.md:245818/5714 ./package.json:245818/2730 ./server.js:245818/4384 ./parallel.js:245818/865

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ You'll get best use out of this project if you're familiar with basic JavaScript
 
 Take a look in `TODO.md` for next steps you can try out in your new site!
 
-___Want a minimal version of this project to build your own Node.js app? Check out [Blank Node](https://glitch.com/edit/#!/remix/glitch-blank-node)!___
+**_Want a minimal version of this project to build your own Node.js app? Check out [Blank Node](https://glitch.com/edit/#!/remix/glitch-blank-node)!_**
 
 ![Glitch](https://cdn.glitch.com/a9975ea6-8949-4bab-addb-8a95021dc2da%2FLogo_Color.svg?v=1602781328576)
 
---
+++ parallel.js
@@ -0,0 +1,57 @@
+const async = require('async');
+
+const callWhenDone = function(err) {
+	if (err) {
+		console.log('ERROR', err);
+	} else {
+		console.log('SUCCESS');
+	}
+}
+
+const doItInParallel = function (callback) {
+	async.parallel([
+		function (done) {
+			console.log('1');
+			done();
+		},
+		function (done) {
+			console.log('2');
+			done();
+		},
+		function (done) {
+			console.log('3');
+			done();
+		},
+		function (done) {
+			setTimeout(function() {
+				console.log('4');
+				done();
+			}, 3000);
+		},
+		function (done) {
+			console.log('5');
+			done();
+		},
+		function (done) {
+			console.log('6');
+			done();
+		},
+		function (done) {
+			console.log('7');
+			done();
+		},
+		function (done) {
+			console.log('8');
+			done();
+		},
+		function (done) {
+			console.log('9');
+			done();
+		}
+	], function (err) {
+		callback(err);
+	});
+}
+
+doItInParallel(callWhenDone);
+
